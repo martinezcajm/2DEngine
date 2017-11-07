67a2a7e8e40a1037aa50bf89800dfa7d96741b76
U-PepePC\Pepe <martinezcajm@esat-alumni.com>
2017-11-07 17:52:29 +0100

    added ui variables

--- 2d_engine/include/game_manager.h
+++ 2d_engine/include/game_manager.h
@@ -7,6 +7,31 @@
 #include "sprite.h"
 #include "scene.h"
 
+typedef enum UiStatus
+{
+  kIdle,
+  kDraw,
+  kSelection,
+  kWrite
+} UiStatus;
+
+typedef enum UiEditType
+{
+  kNull,
+  kRect,
+  kLabel,
+  kSprite,
+  kBackground
+} UiEditType;
+
+typedef enum UiDrawStatus
+{
+  kNotDrawing,
+  kStartDrawing,
+  kDrawing,
+  kStopDrawing
+} UiDrawStatus;
+
 class GameManager {
 private:
   GameManager();
@@ -16,10 +41,10 @@ private:
 public:
   static GameManager& instance();
 
+  uint8_t click_; // indicates if there has been a click
   uint32_t score_;
   // saveGame
   // loadGame
-  
   bool game_over_;
   bool is_editor_;
   Window *window_;
@@ -29,6 +54,10 @@ public:
   Label *label_test_;
   Sprite *sprite_test_;
   sf::Texture *texture_;
+  sf::Vector2f mouse_position_;
+  UiStatus status_ui_;
+  UiEditType edit_type_ui_;
+  UiDrawStatus draw_status_ui_;
 };
 
 #endif
--- 2d_engine/src/game_manager.cc
+++ 2d_engine/src/game_manager.cc
@@ -5,8 +5,13 @@ GameManager::GameManager(){
   window_size_->x = 800;
   window_size_->y = 800;
   window_ = Window::CreateWindow(*window_size_, "My Window");
-
+  status_ui_ = UiStatus::kIdle;
+  edit_type_ui_ = UiEditType::kNull;
   game_over_ = false;
+  draw_status_ui_ = UiDrawStatus::kNotDrawing;
+  click_ = 0;
+  mouse_position_.x = 0;
+  mouse_position_.y = 0;
 }
 
 GameManager& GameManager::instance(){
