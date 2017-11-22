346304094d765a216c5ea35de4b301c24da702d4
martinezcajm <martinezcajm@esat-alumni.com>
2017-11-22 13:30:41 +0100

    first version of user_interface class

--- 2d_engine/build/compilacion_d.bat
+++ 2d_engine/build/compilacion_d.bat
@@ -17,7 +17,8 @@ cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\deps\imgui-sfml\imgui-SFML.cpp -I . -I ..
 cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\pool.cc -I ..\include -I ..\deps\SFML\include
 cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\window.cc -I ..\include -I ..\deps\SFML\include
 cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\game_manager.cc -I ..\include -I ..\deps\SFML\include -I ..\deps\tinyfiledialogs
-cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\scene.cc -I ..\include -I ..\deps\SFML\include -I ..\deps\json-develop\src\ -I ..\deps\tinyfiledialogs
+cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\user_interface.cc -I ..\deps  -I ..\deps\SFML\include -I ..\include -I ..\deps\imgui -I ..\deps\imgui-sfml -I ..\deps\tinyfiledialogs
+cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\scene.cc -I ..\include -I ..\deps\SFML\include -I ..\deps\json-develop\src\ -I ..\deps\tinyfiledialogs -I ..\deps\imgui -I ..\deps\imgui-sfml
 cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\game.cc -I ..\deps  -I ..\deps\SFML\include -I ..\include -I ..\deps\imgui -I ..\deps\imgui-sfml -I ..\deps\tinyfiledialogs
 cl /nologo /Zi /GR- /EHs /W4 /c /MD ..\src\main_game.cc   -I ..\include -I ..\deps\SFML\include -I ..\deps\imgui -I ..\deps\imgui-sfml -I ..\deps\tinyfiledialogs
 cl /nologo /Zi /GR- /EHs /W4 /MD /Fe:..\bin\main_engine.exe main_game.obj entity.obj drawable_entity.obj rect.obj label.obj window.obj game_manager.obj game.obj scene.obj sprite.obj background.obj pool.obj native_dialogs.obj tinyfiledialogs.obj imgui.obj imgui_demo.obj imgui_draw.obj imgui-SFML.obj ..\deps\SFML\lib\sfml-graphics.lib ..\deps\SFML\lib\sfml-window.lib ..\deps\SFML\lib\sfml-system.lib opengl32.lib shell32.lib user32.lib gdi32.lib Comdlg32.lib Ole32.lib
---
+++ 2d_engine/include/user_interface.h
@@ -0,0 +1,83 @@
+// user_interface.h
+// Jose Maria Martinez
+// Header of the functions to control the user interface of the app
+#ifndef __USER_INTERFACE_H__
+#define __USER_INTERFACE_H__ 1
+
+#include "game_manager.h"
+#include "pool.h"
+
+class UserInterface{
+ public:
+  /** @Renders the UI
+  *
+  * Function in charge of rendering the UI
+  *
+  * @return void
+  */
+  void renderUI();
+  /** @Loads the edit values of Drawable entity
+  *
+  * Loads the edit values of Drawable entity: transformations, color, 
+  * position...
+  *
+  * @return void
+  * @param reference to the drawable entity whose values will be loaded at the 
+  * UI
+  */
+  void UiLoadCommonValuesEdit(DrawableEntity &d_entity);
+  /** @Loads the edit values of Rect entity
+  *
+  * Loads the edit values of Rect entity: fill color, is_solid, size...
+  *
+  * @return void
+  * @param reference to the rect whose values will be loaded at the UI
+  * 
+  */
+  void UiLoadRectValuesEdit(Rect &rect);
+  /** @Loads the edit values of Label entity
+  *
+  * Loads the edit values of Label entity: text, font_size...
+  *
+  * @return void
+  * @param reference to the label whose values will be loaded at the UI
+  * 
+  */
+  void UiLoadLabelValuesEdit(Label &label);
+  /** @Loads the edit values of Background entity
+  *
+  * Loads the edit values of Background entity: speed, vertical movememnt, 
+  * horizaontal_movement...
+  *
+  * @return void
+  * @param reference to the background whose values will be loaded at the UI
+  * 
+  */
+  void UiLoadBackgroundValuesEdit(Background &bg);
+  /** @Loads the mode menu of the UI
+  *
+  * Loads the mode menu with the different buttons to chose in which mode is
+  * working the UI
+  *
+  * @return void
+  * 
+  */
+  void UiLoadMenu();
+  /** @Loads the game mode menu
+  *
+  * Only part of the UI that will be shown in game mode, this allows us to
+  * change between game mode and edit mode
+  *
+  * @return void
+  * 
+  */
+  void UiStartGameMenu();
+
+  GameManager& GM = GameManager::instance();
+  Pool& POOL = Pool::instance();
+ private:
+  char const *kFilterPatternsJson[1] = { "*.json" };
+  char const *kFilterPatternsImage[3] = { "*.png","*.jpeg", "*.jpg" };
+};
+
+#endif
