509b9ce01063dbedb78776cc8b7ba10c7d2d677a
U-PepePC\Pepe <martinezcajm@esat-alumni.com>
2017-12-21 00:40:06 +0100

    fixed label update, using sprintf_s now

--- 2d_engine/src/label.cc
+++ 2d_engine/src/label.cc
@@ -71,11 +71,11 @@ void Label::unuse(){
 
 void Label::update(){
   if(tag_ == 10){
-    memcpy(&text_, &GM.score_, 4);
+    sprintf_s(text_, sizeof(text_), "%d", GM.score_);
   }else if(tag_ == 11){
-    memcpy(&text_, &GM.hight_score_, 4);
+    sprintf_s(text_, sizeof(text_), "%d", GM.hight_score_);
   }else if(tag_ == 12){
-    memcpy(&text_, &GM.lives_, 4);
+    sprintf_s(text_, sizeof(text_), "%d", GM.lives_);
   }
 
 }
